5014a23fb8520761c1ccfa040fdb5b712557ab72
Emily Ackerman <emily.ackerman626@gmail.com>
2020-03-12 13:33:56 -0500

    got layout to display

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express'); // first need to npm install express
 const app = express(); // initialize express
 const morgan = require('morgan'); // first need to npm install morgan
+const views = require('./views');
 
-app.use(morgan('dev')); // use morgan with dev setting??
+app.use(morgan('dev')); // use morgan with dev setting?? morgan consoles out GET requests to terminal
 app.use(express.urlencoded({extended: false})); // translates funny url formatting into standard utf-8 characters
 app.use(express.static(__dirname + '/public')); // allows access to the /<currentdirectory>/public folder (can hold .css, logo images, etc)
 
 app.get('/', (req, res, next) => {
-    res.send('Hello World!');
+    res.send(views.main(''));
 })
 
 const PORT = 8080; // defines a port, arbitrary number, go to localhost:8080 in browser
 
-// makes your port listen to the res.send defined above, will log message to terminal 
+// makes your port start listening, will display the res.send defined above, will log message below to terminal 
 app.listen(PORT, () => {
     console.log(`App listening in port ${PORT}`)
 })
